14605f4e6d25ec53829482f44a046766cf4b172d
abdellahaitdahmou <2003091500315@ofppt-edu.ma>
2024-04-25 22:22:45 +0100

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { TypeAnimation } from "react-type-animation";
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './App.css';
+import "./App.css";
 
 function App() {
   const [isVisible, setIsVisible] = useState(true);
@@ -21,12 +21,12 @@ function App() {
       clearTimeout(timeoutId);
     };
 
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('mouseenter', handleMouseEnter);
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mouseenter", handleMouseEnter);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
-      window.removeEventListener('mouseenter', handleMouseEnter);
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("mouseenter", handleMouseEnter);
       clearTimeout(timeoutId);
     };
   }, []);
@@ -38,27 +38,37 @@ function App() {
     autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
 
   return (
     <div className="App">
-      <div className='bg-green-700 text-white h-16 flex justify-center items-center font-bold text-2xl m-0 relative'>
+      <div className="bg-green-200 text-white h-16 flex justify-center items-center font-bold text-2xl m-0 relative">
         <h2>الشحن مجاني و الدفع عند الإستلام</h2>
       </div>
-      <header className={`fixed top-0 left-0 right-0 z-10 bg-[#f1f1f1] shadow-md transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-      <div className='container mx-auto'>
-  <div className='flex justify-between items-center'>
-    <img src="logo.png" alt="Logo" className='w-16 h-auto' /> {/* Ajustez la taille selon vos besoins */}
-    <button className="btn btn-lg bg-warning">أطلب الان</button>
-  </div>
-</div>
-
+      <header
+        className={`fixed top-0 left-0 right-0 z-10 bg-[#f1f1f1] shadow-md transition-opacity duration-300 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div className="container mx-auto">
+          <div className="flex justify-between items-center pt-4 pb-2  px-5">
+            <img src="logo.png" alt="Logo" className="w-[45%] h-auto" />{" "}
+            {/* Ajustez la taille selon vos besoins */}
+            <button
+              id="myButton"
+              className="btn p-3 rounded-3xl text-white w-[150px] font-bold text-xl shadow-lg"
+            >
+              أطلب الآن
+            </button>
+          </div>
+        </div>
       </header>
-      <div className="text-center text-bold font-Poppins text-4xl md:text-5xl lg:text-6xl mb-8 md:mb-12 lg:mb-16 p-4 md:p-9">
-        <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl">مصباح متعدد الوظائف يجمع بين</h1>
-        <br />
-        <b className="mt-3 bg-first px-6 py-3 rounded-full inline-block border-2 border-second">
+      <div className="text-center text-bold font-Poppins text-4xl md:text-5xl lg:text-6xl mb-0 md:mb-12 lg:mb-16 p-4 md:p-9 mt-5 ">
+        <h1 className="text-4xl font-bold md:text-5xl lg:text-6xl text-gray-500">
+          مصباح متعدد الوظائف يجمع بين
+        </h1>
+        <p className="mt-3 bg-first px-4 py-1 rounded-full inline-block border-2 border-second">
           <TypeAnimation
             sequence={[
               " الجودة العالية",
@@ -78,14 +88,18 @@ function App() {
             }}
             repeat={Infinity}
           />
-        </b>
+        </p>
       </div>
       <div className="w-full flex justify-center px-5">
         <div className="w-full md:w-3/4 max-w-screen-md">
           <Slider {...settings}>
-            {[1, 2, 3, 4, 5, 6, 7, 8].map(index => (
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((index) => (
               <div key={index}>
-                <img src={`/image${index}.png`} alt={`description ${index}`} className="w-full h-auto rounded-3xl yassine" />
+                <img
+                  src={`/image${index}.png`}
+                  alt={`description ${index}`}
+                  className="w-full h-auto rounded-3xl yassine"
+                />
               </div>
             ))}
           </Slider>
