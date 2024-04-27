25034b1d9c1f3bd09224ecec1b8193541bff4c0a
abdellahaitdahmou <2003091500315@ofppt-edu.ma>
2024-04-27 05:19:13 +0100

    v5 full landing page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -10,11 +10,11 @@ function Footer() {
           </div>
         </div>
       </div>
-      <div className="flex justify-center mb-5 text-gray-500 text-xl font-bold">
+      <div className="flex justify-center text-center mb-5 text-gray-500 text-xl font-bold">
         <p> الشحن مجاني إلى باب المنزل, مع إمكانية الإسترجاع</p>
       </div>
       <div className="flex justify-center">
-        <div className="text-right bg-gray-500 text-white w-[100%]">
+        <div className="text-center bg-gray-500 text-white w-[100%]">
           <h4>&copy;2024/2025</h4>
         </div>
       </div>
